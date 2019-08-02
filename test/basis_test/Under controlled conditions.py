7d1a29254a05f801bcff630de336a284221c2f29
ClivenZ <835105614@qq.com>
2019-08-02 22:32:03 +0800

    “basis test”

---
+++ test/basis_test/Under controlled conditions.py
@@ -0,0 +1,20 @@
+'''
+多行注释
+'''
+#if条件语句
+if_see = True
+if if_see:
+    print("maimaimai")
+#if-else条件语句
+if if_see:
+    print("2")
+else:
+    print("1")
+#if-elif.....条件语句
+name = input("please input:")
+if name == "234":
+    print("666")
+elif name == "213":
+    print("777")
+elif name == "123":
+    print("vvv")
---
+++ test/basis_test/test.py
@@ -0,0 +1,23 @@
+#声明一个变量
+#int型
+first_number = 1
+print(first_number)
+print(type(first_number))
+#float型
+second_float_number = 1.2
+print(second_float_number)
+print(type(second_float_number))
+#bool型
+third_boolean = False
+print(third_boolean)
+print(type(third_boolean))
+
+#幂运算
+i = 1
+j = 2
+z = i**j
+print(z)
+
+#取整除运算
+z = i // j
+print(z)
