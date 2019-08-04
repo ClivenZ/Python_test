495a6eb66ce525483dd95a3dc867321f08a46b07
ClivenZ <835105614@qq.com>
2019-08-04 21:45:36 +0800

    0

---
+++ test/basis_test/Bubble_Sort.py
@@ -0,0 +1,28 @@
+#冒泡排序
+# nums = [2, 3, 9, 5, 1, 8, 6, 7, 4]
+# for i in range(len(nums) - 1):
+#     for j in range(len(nums) -i -1):
+#         if nums[j] > nums[j + 1]:
+#             nums[j],nums[j + 1] = nums[j + 1],nums[j]
+# print(str(nums))
+
+#print(nums.sort())
+
+#test
+# num = [3, 1, 25, 10, 15, 6, 8]
+# for i in range(len(num) - 1):
+#     for j in range(len(num) -i -1):
+#         if num[j] < num[j + 1]:
+#           num[j],num[j+1] = num[j+1],num[j]
+# print(str(num))
+# cur = 7
+# for m in range(len(num) - 1):
+#     if cur < num[m]:
+#         cur = num[m + 1]
+#     elif cur > num[m]:
+#         num[m + 1] = num[m]
+#         num[m] = cur
+#         break
+# print(str(num))
+
+import antigravity
